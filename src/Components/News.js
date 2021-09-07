e3abf913b5da800cff20586f535c6e9b0df55994
Atif7355 <78923890+Atif7355@users.noreply.github.com>
2021-09-07 15:27:04 +0530

    Update News.js

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,7 +1,6 @@
 import React, {useEffect, useState} from 'react'
 
 import NewsItem from './NewsItem'
-import Spinner from './Spinner';
 import PropTypes from 'prop-types'
 import InfiniteScroll from "react-infinite-scroll-component";
 
@@ -48,12 +47,11 @@ const News = (props)=>{
         return (
             <>
                 <h1 className="text-center" style={{ margin: '35px 0px', marginTop: '90px' }}>NewsBulletin - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
-                {loading && <Spinner />}
+                {loading}
                 <InfiniteScroll
                     dataLength={articles.length}
                     next={fetchMoreData}
                     hasMore={articles.length !== totalResults}
-                    loader={<Spinner/>}
                 > 
                     <div className="container">
                          
