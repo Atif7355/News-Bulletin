c92a6dc2373c359f5c38785cb2aa72316d82c7b5
Atif7355 <78923890+Atif7355@users.noreply.github.com>
2021-09-07 15:11:51 +0530

    Add files via upload

---
+++ src/Components/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer className="bg-light text-center text-lg-start">
+  <div className="text-center p-3" style={{backgroundColor:"Black",fontFace:"Verdana"}}>
+    <h1 style={{backgroundColor:"White"}}>© 2021 Copyright</h1>
+  </div>
+</footer>
+            </div>
+        )
+    }
+}
+
